3faa110f4bbcc6a0936dab770d601ba7ebb5ffdd
Jerry Hanks <jerryhanksokafor@gmail.com>
2019-06-30 15:00:22 +0100

    feat : record and playback audio

---
+++ pitch-perfect/PlaySoundsViewController.swift
@@ -0,0 +1,85 @@
+//
+//  PlaySoundsViewController.swift
+//  pitch-perfect
+//
+//  Created by Jerry Hanks on 30/06/2019.
+//  Copyright © 2019 Mint Financial Services. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+class PlaySoundsViewController: UIViewController {
+    @IBOutlet weak var slowBtn: UIButton!
+    @IBOutlet weak var fastBtn: UIButton!
+    @IBOutlet weak var hightPitchBtn: UIButton!
+    @IBOutlet weak var lowPitchBtn: UIButton!
+    @IBOutlet weak var echoBtn: UIButton!
+    @IBOutlet weak var reverbBtn: UIButton!
+    @IBOutlet weak var playBackControlBtn: UIButton!
+    
+    var recordedAudioURL:URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode:AVAudioPlayerNode!
+    var stopTimer:Timer!
+    
+    
+    enum FilterTypes : Int{
+        case slow = 0, fast,highPtich,lowPitch,echo,reverb
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.backBarButtonItem = nil
+        self.navigationItem.hidesBackButton = true
+        
+        //set up audio
+        setupAudio()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        configureUI(.notPlaying)
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //make navbar seamles with the view background
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+    }
+    
+    @IBAction func onTapFilterBtn(_ sender: UIButton) {
+        let filterType = FilterTypes(rawValue: sender.tag)!
+        
+        switch filterType {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+             playSound(rate: 1.5)
+        case .highPtich:
+            playSound(pitch:1000)
+        case .lowPitch:
+            playSound(pitch:-1000)
+        case .echo:
+            playSound(echo:true)
+        case .reverb:
+            playSound(reverb:true)
+        }
+        
+        
+        //update the UI
+        configureUI(.playing)
+    }
+    
+    @IBAction func onTapPlayBackControlBtn(_ sender: UIButton) {
+        stopAudio()
+    }
+    
+    
+    @IBAction func onTapRecordNewsoundBtn(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
--- pitch-perfect/RecordSoundViewController.swift
+++ pitch-perfect/RecordSoundViewController.swift
@@ -7,21 +7,119 @@
 //
 
 import UIKit
+import AVFoundation
 
-class RecordSoundViewController: UIViewController {
 
+class RecordSoundViewController: UIViewController,AVAudioRecorderDelegate {
+    @IBOutlet weak var controlBtn: UIButton!
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    var audioRecorder:AVAudioRecorder!
+    
+    
 //    override var prefersStatusBarHidden: Bool{
 //        get{return true}
 //    }
+    
+    enum ControlType:Int {
+        case record = 0,stopRecording
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         //make fullscreens
-        navigationController?.isNavigationBarHidden = true
+//        navigationController?.isNavigationBarHidden = true
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //make navbar seamles with the view background
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+    }
 
+    @IBAction func onTapControlBtn(_ sender: UIButton) {
+        let controlType = ControlType(rawValue: sender.tag)!
+        toggleControlButton(previousType: controlType)
+        
+        switch (controlType) {
+        case .record:
+            print("Recording!")
+            break
+        case .stopRecording:
+            print("Stop Recording")
+            break
+        }
+    }
+    
+    func toggleControlButton(previousType:ControlType){
+        switch previousType {
+        case .record:
+            controlBtn.setImage(UIImage(named: "Stop"), for: .normal)
+            controlBtn.tag = 1
+            infoLabel.text = "Tap to finish recording"
+            
+            startRecording()
+            break
+        case .stopRecording:
+            controlBtn.setImage(UIImage(named: "Record"), for: .normal)
+            controlBtn.tag = 0
+            infoLabel.text = "Tap to start recording"
+            
+            finishRecording()
+            break
+        }
+    }
+    
+    private func startRecording(){
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
+        
+        print("Recording audio at : \(filePath!)")
+        
+        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+        
+    }
+    
+    private func finishRecording(){
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toPlayback"{
+            let playbackVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! URL
+            playbackVC.recordedAudioURL = recordedAudioURL
+            
+        }
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if flag{
+            performSegue(withIdentifier: "toPlayback", sender: audioRecorder.url)
+        }else{
+            print("Recording was not successful")
+        }
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        print("Recording erro: \(String(describing: error))")
+        finishRecording()
+    }
+    
 }
 
